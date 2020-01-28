8f916a5044f8799066270024dfbc29a8267bb9fe
unknown <aarfeenaalia@gmail.com>
2020-01-28 22:40:18 +0530

    added search page feature code

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -3,7 +3,12 @@ package com.qa.pages;
 public class LoginPage {
 
 	
-	public void login() {
+	public void loginPage() {
 		System.out.println("Login page");
 	}
+	
+	public void loginSet() {
+		System.out.println("Login page");
+	}
+	
 }
